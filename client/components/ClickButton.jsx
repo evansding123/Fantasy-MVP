97323d7c0a7c72782832d901e8bc263cc11e1a4a
Evans Ding <evansding@gmail.com>
2021-07-08 16:48:59 -0700

    updated button

--- client/components/ClickButton.jsx
+++ client/components/ClickButton.jsx
@@ -1,15 +1,17 @@
 /* eslint-disable react/prop-types */
 import React, { useState, useEffect } from 'react';
+import Button from '@material-ui/core/Button';
+
 
 const ClickButton = (props) => {
 
-  const handleClick = ()=> {
-    props.callback(event.target.value);
+  const handleClick = (event)=> {
+    props.callback(event.currentTarget.value);
   }
 
   return(
-    <button type = 'button' value = {props.statSort} onClick = {handleClick}>{props.statSort}</button>
-  )
+    <Button color = 'primary' type = 'button' value = {props.statSort} onClick = {handleClick}>{props.statSort}</Button>
+  );
 
 
 }
